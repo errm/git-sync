92b68a8210c718eb4a1573eb1e0b2a437b49fd3a
Ed Robinson <ed.robinson@reevoo.com>
2017-08-29 11:20:47 +0100

    Inital Implimentation

---
+++ README.md
@@ -0,0 +1,12 @@
+# `git-sync`
+
+Sync from a remote git repo every so often.
+
+## Config
+
+| var          | default          |
+|--------------|------------------|
+| `TARGET_DIR` | `/usr/local/src` |
+| `GIT_REPO`   |                  |
+| `GIT_BRANCH` | `master`         |
+| `DELAY`      | `1m`             |
---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -xe
+
+if [ ! -d $TARGET_DIR ]; then
+    mkdir -p $TARGET_DIR
+fi
+
+if [ -z $GIT_REPO ]; then
+  echo "The GIT_REPO environment variable must be set"
+  exit 1
+fi
+
+git clone $GIT_REPO $TARGET_DIR
+
+if [ ! -z $GIT_BRANCH ]; then
+  git checkout $GIT_BRANCH
+fi
+
+while true; do
+  git pull || git reset --hard
+  sleep $DELAY
+done
